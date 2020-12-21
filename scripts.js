d15aa31f840b58b69453f14ecf59b632b596c7d8
GarySimons <garycsimons@gmail.com>
2020-12-21 06:32:57 +0000

    set up countdown global variable

--- scripts.js
+++ scripts.js
@@ -1,8 +1,22 @@
+// global variable to store setInterval on screen
+let countdown; 
+
+
 function timer(seconds) {
     // get the current time to know when timer starts
     const now = Date.now();
-    // how long to run it for
+    // how long to run it for (when it stops)
     const then = now + seconds * 1000;
-    consolelogo({now, then});
+    
+    // set it to run every second
+    countdown = setInterval(() => {
+        // how much time left on clock. Math.round rounds the numbers to whole
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        // check to see if we should stop it if secondsLeft is less than 0
+        if(secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
 
+    }, 1000);
 }
